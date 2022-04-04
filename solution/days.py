67a9e631b3acdcbad90ed238d48e2035f76b3ca7
21bca29 <96609325+21bca29@users.noreply.github.com>
2022-04-04 11:10:23 +0530

    Add files via upload

---
+++ solution/days.py
@@ -0,0 +1,17 @@
+a=int(input('Enter any number:'))
+if(a==1):
+    print('It is Monday')
+elif(a==2):
+    print('It is Tuesday')
+elif(a==3):
+    print('It is Wedensday')
+elif(a==4):
+    print('It is Thursday')
+elif(a==5):
+    print('It is Friday')
+elif(a==6):
+    print('It is Saturday')
+elif(a==7):
+    print('It is Sunday')
+else:
+    print('There are no such days')
